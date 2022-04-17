234655189aa0c131a26b1dec5342718452585ac8
Ilya Prokopenko <dsrev@protonmail.com>
2022-04-17 09:00:39 +0300

    Global: Add verification for users and groups
    Resolves #18

--- res/podrobnee.js
+++ res/podrobnee.js
@@ -2,7 +2,7 @@ let p_bt = document.querySelector('.podrobnee');
 let p_content = document.querySelector('.podrobnee_content');
 
 p_bt.addEventListener('click', function(){
-	let text = ["Показать подробнее", "Скрыть интим"];
+	let text = ["Показать подробнее", "Скрыть подробности"];
 	p_bt.innerText = text[Number(p_bt.innerText === text[0])]
 	p_content.classList.toggle('active');
 })
